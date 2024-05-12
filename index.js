75882feb6fc23b7f2a55e513e256c1b6d12d19a7
Lucidest <59543168+EpicBacon123@users.noreply.github.com>
2024-05-12 18:56:28 -0400

    fixed .a1

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ function a1() {
     let num = 0;
 
     let pick1 = Math.random();
+    // img.src=images1[0];
     if (pick1 < .12) {
         num = Math.floor(Math.random() * images2.length);
         img.src = images2[num];
@@ -46,20 +47,21 @@ function a1() {
 }
 
 const images1 = [
-    "https://media.discordapp.net/attachments/782347772854534154/826490976599801907/unknown.png?ex=66390334&is=6637b1b4&hm=7faf07c92c291dd5d7e6a3579e8b7918d167822f008d71931d55242c0d8c28d8&=&format=webp&quality=lossless&width=264&height=226",
-    "https://media.discordapp.net/attachments/782347772854534154/826489892736860260/unknown.png?ex=66390231&is=6637b0b1&hm=0f7e7b02d0bac97aff3dc2603d10a58fd75fc4b677bb497a710d44ab22ac67ee&=&format=webp&quality=lossless&width=148&height=114",
-    "https://media.discordapp.net/attachments/782347772854534154/826489760197378058/unknown.png?ex=66390212&is=6637b092&hm=254a08873f1304ed199b508848d3763976180efd545b53ab3a1fd5c75d7add9d&=&format=webp&quality=lossless&width=301&height=186",
-    "https://media.discordapp.net/attachments/782347772854534154/826489699903602808/unknown.png?ex=66390203&is=6637b083&hm=a35d80daea2b9c49b9460e4c90e6f0c07968af9d7dd2de121dc195d6f34df487&=&format=webp&quality=lossless&width=267&height=127",
-    "https://media.discordapp.net/attachments/782347772854534154/826489094766723082/unknown.png?ex=66390173&is=6637aff3&hm=0cef3539db4b584c61deb6629a3ca55395bf673c227fc64b0257f6aa29808c23&=&format=webp&quality=lossless&width=210&height=225",
-    "https://media.discordapp.net/attachments/782347772854534154/826488814767571006/unknown.png?ex=66390130&is=6637afb0&hm=7e561077de33144df282ac558005bf33722b2a91220d7e45dcd0c02f84b88777&=&format=webp&quality=lossless&width=199&height=225",
-    "https://media.discordapp.net/attachments/782347772854534154/826488712128757861/unknown.png?ex=66390118&is=6637af98&hm=2dc674c60c853b17e444b92a77a38ef67255a0d20ea9433d3296950d6ceb9ae9&=&format=webp&quality=lossless&width=51&height=127",
-    "https://media.discordapp.net/attachments/782347772854534154/929193600439422996/unknown.png?ex=6638e339&is=663791b9&hm=b9da1490f61657a5c2784168a3bd12b0058baec56be9d5e3f18d0ea97be4dd22&",
-    "https://media.discordapp.net/attachments/782347772854534154/838831366358958190/unknown.png?ex=66386c56&is=66371ad6&hm=e0dcda4efa6a3c4f01c3c6a2b9027aa71beff1386749ee6121e05618bfa9298b&=&format=webp&quality=lossless&width=807&height=510",
-    "https://media.discordapp.net/attachments/782347772854534154/903831271623491604/unknown.png?ex=6638e7b7&is=66379637&hm=063d82f943e966ba9a587d3d1bc091692d39130140c27d4215921013be7c5bea&=&format=webp&quality=lossless&width=1084&height=577"
+    "https://cdn.discordapp.com/attachments/782347772854534154/826488814767571006/unknown.png?ex=66423bb0&is=6640ea30&hm=a89857e60f0375ac3325bf880f447d7fdf50aa3e7f4a8b79e59e070f30a09b8e&",
+    "https://cdn.discordapp.com/attachments/782347772854534154/826488712128757861/unknown.png?ex=66423b98&is=6640ea18&hm=b220c79d61796d65ad40ee47bbd6028be2e9e0d860194649e24a0866f13ce3df&",
+    "https://cdn.discordapp.com/attachments/782347772854534154/826489094766723082/unknown.png?ex=66423bf3&is=6640ea73&hm=a9219140b68836074ca3be01d09b3135f029e1906f526b9ced1368a056ceea24&",
+    "https://cdn.discordapp.com/attachments/782347772854534154/826489699903602808/unknown.png?ex=66423c83&is=6640eb03&hm=6418a0ea0bfd540d5b797454ff4dcdf4ad3769ffd105c1faf640c6d999064f21&",
+    "https://cdn.discordapp.com/attachments/782347772854534154/826490976599801907/unknown.png?ex=66423db4&is=6640ec34&hm=ee71a3a61c6f002172518ff5ab0803f73c9df905d3104d44e80a5975ff42dbfb&",
+    "https://media.discordapp.net/attachments/782347772854534154/974448573347405864/unknown.png?ex=6641f534&is=6640a3b4&hm=d16e34de110bf462534ef1fa5692726cb799dc2b2160b9abe2b2c7281f584081&",
+    "https://media.discordapp.net/attachments/782347772854534154/983070513905692712/IMG_20220605_141018.jpg?width=446&height=594&ex=664257c3&is=66410643&hm=5107b4b7bff61ced1f78cd26920925f90ef644b1ee7675ebe56088c4e1721eb6&",
+    "https://media.discordapp.net/attachments/782347772854534154/903831271623491604/unknown.png?width=1115&height=594&ex=66422237&is=6640d0b7&hm=ca907cf007e4cf7e9262ff59bdeade212d7a5b75fa115768770f648961e60637&",
+    "https://media.discordapp.net/attachments/782347772854534154/926148337130930186/unknown.png?ex=66423e5a&is=6640ecda&hm=4e173b471dff8a309bcae5baa4da201666dff4c6c3cf723da83b498938459fc5&",
+    "https://media.discordapp.net/attachments/782347772854534154/929192816251387914/unknown.png?ex=66421cfe&is=6640cb7e&hm=7d7e9f650f435990a23ec02857eb822cf1abffd46699a0ea28e4922b2371bc59&",
+    "https://media.discordapp.net/attachments/782347772854534154/929193600439422996/unknown.png?ex=66421db9&is=6640cc39&hm=01366f8844e03af3d72d57efe88d8ba59be84fae7e3a55f53525ed4245ef3f1e&",
 ]
 
 const images2 = [
-    "https://media.discordapp.net/attachments/782347772854534154/826489193521610862/unknown.png?ex=6639018b&is=6637b00b&hm=4552c369e5a7d1edcbf9ef594017983173abba41582cf7692c4f322857db142d&=&format=webp&quality=lossless&width=319&height=349",
-    "https://media.discordapp.net/attachments/782347772854534154/826488792323194939/unknown.png?ex=6639012b&is=6637afab&hm=30874728c6d4de07868a4ce799f1f2b426fe0176c298c4887596c06be8b3fe53&=&format=webp&quality=lossless&width=433&height=330",
-    "https://media.discordapp.net/attachments/782347772854534154/826487903571935262/unknown.png?ex=66390057&is=6637aed7&hm=0944190d59ca1727c96154dd28a1dafb0fb61f5959824346fbb440209374d4e8&=&format=webp&quality=lossless&width=145&height=177",
+    "https://cdn.discordapp.com/attachments/782347772854534154/826487903571935262/unknown.png?ex=66423ad7&is=6640e957&hm=87a3d8a41e7e0fc3764503ee6c470f4478092866602ba0d7e2b75d081b9b4339&",
+    "https://cdn.discordapp.com/attachments/782347772854534154/826489193521610862/unknown.png?ex=66423c0b&is=6640ea8b&hm=d6004fd766273c0fec87696725670c3ed9e1b1effd62c8daf8ffb60c21a5cd5b&",
+    "https://cdn.discordapp.com/attachments/782347772854534154/826489892736860260/unknown.png?ex=66423cb1&is=6640eb31&hm=2afb9a17329acb2289f5a1107b4bba590f9f803e0c6b4721adc816ad80ad5ccd&",
 ]
